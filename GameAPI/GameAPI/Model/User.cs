782c460b7bf77f21bfda6ed93d052a92e7b05568
its7ony <its7ony@DESKTOP-16G1T07>
2020-05-21 01:04:57 -0500

    Aquí se encuentra la API y la base de datos necesaria para que funcione

---
+++ GameAPI/GameAPI/Model/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GameAPI
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Pwd { get; set; }
+        public string Created { get; set; }
+        public int Score { get; set; }
+    }
+}
